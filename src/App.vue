2d8ef65613bfb490b2b9e86ab285c3981804373d
Leandro Ruel <leandro.ruel@gmail.com>
2021-01-16 23:15:55 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Navbar />
+</template>
+
+<script>
+import Navbar from "./components/Navbar";
+
+export default {
+  name: "App",
+  components: {
+    Navbar
+  }
+};
+</script>
+
+<style>
+html,
+body {
+  height: 100%;
+  font-size: 16px;
+}
+</style>
